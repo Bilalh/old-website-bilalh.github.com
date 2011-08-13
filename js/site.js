59b180cd0181dcf28f4b85a417ae94dbbccbba7e
Bilal Hussain <bhterra@gmail.com>
2011-08-13 15:08:53 +0100

    first commit

---
+++ js/site.js
@@ -0,0 +1,21 @@
+function getEmail()
+{
+  var nf323 = "-fusion";
+  var py23432 = "ael";
+  var px13452 = "mich";
+  var pw3 = ".com";
+  var a343t = "@nova";
+  return px13452 + py23432 + a343t + nf323 + pw3;
+}
+
+function removeLinkListeners()
+{
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++)
+  {
+    if (links[i].classList.contains('project-download-link'))
+    {
+      links[i].removeEventListener('mousedown', clicky.outbound);
+    }
+  }
+}
