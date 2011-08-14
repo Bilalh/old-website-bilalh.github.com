b38ca3b54490479c6c4d2e8f6a851ab095fb70ad
Bilal Hussain <bhterra@gmail.com>
2011-08-14 01:47:03 +0100

    project changelog

--- _plugins/projects.rb
+++ _plugins/projects.rb
@@ -8,14 +8,14 @@ module Jekyll
 			super site, base, dir, 'project'
 			puts "Building project: #{name}"
 			
-			self.data['title'] = info['title'] || name
-			self.data['version'] = info['version_title'] || info['version']
-			self.data['repo'] = "https://github.com/#{site.config['github_user']}/#{name}"
-			self.data['download'] = "#{self.data['repo']}/zipball/#{info['version'] || 'master'}"
-			self.data['docs'] = info['docs'] == 'wiki' ? "#{self.data['repo']}/wiki" : info['docs'] if info['docs']
+			self.data['title']       = info['title'] || name
+			self.data['version']     = info['version_title'] || info['version']
+			self.data['repo']        = "https://github.com/#{site.config['github_user']}/#{name}"
+			self.data['download']    = "#{self.data['repo']}/zipball/#{info['version'] || 'master'}"
+			self.data['docs']        = info['docs'] == 'wiki' ? "#{self.data['repo']}/wiki" : info['docs'] if info['docs']
 			self.data['description'] = info['description']
 			
-			filename="_cache/#{name}"
+			filename="_cache/#{name}.readme"
 			if !File.exists?(filename) then
 				# this stuff is bit hackish, but it works
 				# this will fail if README.md isn't present
@@ -27,8 +27,31 @@ module Jekyll
 				readme = IO.read filename
 			end
 			
-			self.data['readme'] = RDiscount.new(readme).to_html
+			self.data['readme']    = RDiscount.new(readme,:generate_toc).to_html
+			# self.data['readme']    =  readme
+			self.data['changelog'] = 'changelog.html'
 		end
+	end
+	
+	class ChangeLog < CustomPage
+		def initialize(site, base, dir, name, project)
+			super site, base, dir, 'changelog', 'changelog.html'
+			['title', 'version','repo','download'].each do |key|
+				self.data[key] = project.data[key]
+			end
+			
+			filename="_cache/mplayer-last.fm-scrobbler.changelog"
+			
+			# filename="_cache/#{name}.readme"
+			changelog = IO.read filename
+			changelog.gsub!(/\`{3} ?(\w+)\n(.+?)\n\`{3}/m, "{% highlight \\1 %}\n\\2\n{% endhighlight %}")
+			changelog = Liquid::Template.parse(changelog).render(
+				{}, :filters => [Jekyll::Filters], :registers => { :site => site }
+			)
+			
+			self.data['changelog'] = changelog
+		end	
+		
 	end
 	
 	class Projects < CustomPage
@@ -48,17 +71,18 @@ module Jekyll
 			projects = []
 			Dir.mkdir '_cache' unless  File.directory?('_cache')
 			
-			
 			self.config['projects'].each do |k, v|
 				slug = v['slug'] || k.slugize
 				p = Project.new(self, self.source, File.join(dir, slug), k, v)
 				p.data['url'] = "/#{dir}/#{slug}"
 				projects << p
 				write_page p
+				write_page ChangeLog.new(self, self.source, File.join(dir, slug),k, p)
 			end
 			
 			write_page Projects.new(self, self.source, dir, projects) if self.layouts.key? 'projects'
 		end
 	end
+	
 end
  
