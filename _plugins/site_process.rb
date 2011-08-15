998b1d7f5ff74b3a1576b4654cd8b0cfbd29618d
Bilal Hussain <bhterra@gmail.com>
2011-08-15 21:49:42 +0100

    pagagation working with blog

--- _plugins/site_process.rb
+++ _plugins/site_process.rb
@@ -2,24 +2,34 @@
 require 'growl'
 
 module Jekyll
-  class Site
-    def process
-      self.reset
-      self.read
-      self.generate
-      self.render
-      
-      # these must come after render
-      self.generate_tags_categories
-      self.generate_archives
-      self.generate_projects
-      
-      self.cleanup
-      self.write
-      
-      # Growl
-      Growl.notify "Build complete.", :title => "Jekyll"
-    end
-  end
+	class Site
+		def process
+			self.reset
+			self.read
+			self.generate
+
+						
+			# gindex = self.generators.index do |x|
+			# 	x.class == Pagination
+			# end
+			#         
+			# pindex = pages.index do |x|
+			# 	x.real_dir == '/blog' && x.basename = 'index.html'
+			# end               
+			# self.generators[gindex].paginate(self, pages[pindex]);    
+			
+			self.render 
+			# these must come after render
+			self.generate_tags_categories
+			self.generate_archives
+			self.generate_projects
+			self.cleanup
+			self.write
+			
+			
+			# Growl
+			Growl.notify "Build complete.", :title => "Jekyll"
+		end
+	end
 end
  
