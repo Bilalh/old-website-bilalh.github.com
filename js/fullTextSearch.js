b76482369f0d86585e9856a0ee46abdd9bf6fda9
Bilal Hussain <bhterra@gmail.com>
2011-08-16 03:43:00 +0100

    searching

--- js/fullTextSearch.js
+++ js/fullTextSearch.js
@@ -44,8 +44,8 @@ function search(query) {
 	var result = $.getJSON('http://dcz51.api.indextank.com/v1/indexes/BsBlog/search?q=' + encodeURIComponent(query) + '&fetch=title&snippet=text&callback=?', function(data) {
 		$.each(data.results, function(index, result) {
 			$('div#results').html('<div id="result">\
-					<p><a href="http://bilalh.github.com/' + result.docid + '">' + result.title + '</a><br/>\
-					' + result.snippet_text + '</p>\
+					<p><a href="http://bilalh.github.com/' + result.docid + '">' + result.title + '</a><br/>' 
+					+ result.snippet_text + '</p> <p><a href="' + "http://bilalh.github.com/" + result.docid +   '">Read more...</a></p>\
 			</div')
 		});
 	});
