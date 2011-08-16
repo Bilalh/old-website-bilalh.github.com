e3ccafc4fbadc7ffd59e5e5cf3b4d9d25b1e72c5
Bilal Hussain <bhterra@gmail.com>
2011-08-16 02:12:46 +0100

    rss and full text search

--- js/fullTextSearch.js
+++ js/fullTextSearch.js
@@ -1,8 +1,34 @@
+
 $(document).ready(function() {
 	$('form#search').submit(function() {
 			search($('input#query').val());
 		return false;
 	}); 
+	
+	// Add the value of "Search..." to the input field and a class of .empty
+	$("#query").val("Search Posts...").addClass("empty");
+
+	// When you click on #search
+	$("#query").focus(function(){
+
+		// If the value is equal to "Search..."
+		if($(this).val() == "Search Posts...") {
+			// remove all the text and the class of .empty
+			$(this).val("").removeClass("empty");;
+		}
+
+	});
+
+	// When the focus on #search is lost
+	$("#query").blur(function(){
+
+		// If the input field is empty
+		if($(this).val() == "") {
+			// Add the text "Search..." and a class of .empty
+			$(this).val("Search Posts...").addClass("empty");
+		}
+
+	});
 });
 
 function search(query) {
