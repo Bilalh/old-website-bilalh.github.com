81248aa9969fea8c246c399248c722294c84caba
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2011-12-31 04:52:57 +0000

    2011-12-31_04-52_1325307177

--- js/site.js
+++ js/site.js
@@ -13,44 +13,44 @@ function makeEmail()
 
 $(document).ready(function() {
      
-    // Add the value of "Search..." to the input field and a class of .empty
-    $(".gsc-input").val("Search...").addClass("empty");
- 
-    // When you click on #search
-    $(".gsc-input").focus(function(){
- 
-        // If the value is equal to "Search..."
-        if($(this).val() == "Search...") {
-            // remove all the text and the class of .empty
-            $(this).val("").removeClass("empty");;
-        }
-        this.select();
-    });
- 
-    // When the focus on #search is lost
-    $(".gsc-input").blur(function(){
- 
-        // If the input field is empty
-        if($(this).val() == "") {
-            // Add the text "Search..." and a class of .empty
-            $(this).val("Search...").addClass("empty");
-        }
- 
-    });
+    // // Add the value of "Search..." to the input field and a class of .empty
+    // $(".gsc-input").val("Search...").addClass("empty");
+    //  
+    // // When you click on #search
+    // $(".gsc-input").focus(function(){
+    //  
+    //     // If the value is equal to "Search..."
+    //     if($(this).val() == "Search...") {
+    //         // remove all the text and the class of .empty
+    //         $(this).val("").removeClass("empty");;
+    //     }
+    //     this.select();
+    // });
+    //  
+    // // When the focus on #search is lost
+    // $(".gsc-input").blur(function(){
+    //  
+    //     // If the input field is empty
+    //     if($(this).val() == "") {
+    //         // Add the text "Search..." and a class of .empty
+    //         $(this).val("Search...").addClass("empty");
+    //     }
+    //  
+    // });
     $('a#mail').prop("href", makeEmail())
 
 });
  
-
-$(function() {
-    $(".gsc-input").focus(function(){
- 
-        // If the value is equal to "Search..."
-        if($(this).val() == "Search...") {
-            // remove all the text and the class of .empty
-            $(this).val("").removeClass("empty");;
-        }
-        this.select();
-    });
-});
-
+// 
+// $(function() {
+//     $(".gsc-input").focus(function(){
+//  
+//         // If the value is equal to "Search..."
+//         if($(this).val() == "Search...") {
+//             // remove all the text and the class of .empty
+//             $(this).val("").removeClass("empty");;
+//         }
+//         this.select();
+//     });
+// });
+// 
