715c555801800d4e76154bb2663dbe6b0ca46951
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2011-12-31 04:38:11 +0000

    2011-12-31_04-38_1325306291

--- js/site.js
+++ js/site.js
@@ -10,16 +10,34 @@ function makeEmail()
   return rdfsdf+px13452+py23432+nf323+fdsff+a343t+dsada;
 }                           
 
-function removeLinkListeners()
-{
-  var links = document.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++)
-  {
-    if (links[i].classList.contains('project-download-link'))
-    {
-      links[i].removeEventListener('mousedown', clicky.outbound);
-    }
-  }
-}
 
-
+$(document).ready(function() {
+     
+    // Add the value of "Search..." to the input field and a class of .empty
+    $("#gsc-input").val("Search...").addClass("empty");
+ 
+    // When you click on #search
+    $("#gsc-input").focus(function(){
+ 
+        // If the value is equal to "Search..."
+        if($(this).val() == "Search...") {
+            // remove all the text and the class of .empty
+            $(this).val("").removeClass("empty");;
+        }
+        this.select();
+    });
+ 
+    // When the focus on #search is lost
+    $("#gsc-input").blur(function(){
+ 
+        // If the input field is empty
+        if($(this).val() == "") {
+            // Add the text "Search..." and a class of .empty
+            $(this).val("Search...").addClass("empty");
+        }
+ 
+    });
+    $('a#mail').prop("href", makeEmail())
+ 
+});
+ 
