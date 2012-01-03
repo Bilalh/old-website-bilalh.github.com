bc887f477636a608861e1771a9d055bb9308fcbe
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-01-03 00:13:33 +0000

    2012-01-03_00-13_1325549613

--- compress.rb
+++ compress.rb
@@ -3,12 +3,12 @@ require "front-compiler"
 
 class FrontCompiler::HTMLCompactor
 	  def remove_trailing_spaces(source)
-		source.gsub /(\n[ \t]*)+/, ""
+		source.gsub /(\n[ \t]*)+/, "\n"
 		end
 end
 
 c = FrontCompiler.new
-Dir.glob('_compiled/**/*.{html}').each do |f|
+Dir.glob('_site/**/*.{html,css,js}').each do |f|
 	compressed = c.compact_file(f)
 	File.open(f, "w") { |file| file.write compressed }
 end
