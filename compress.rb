b264bc426a31d69626f95fdc3c4d97a6945155e9
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-01-03 01:44:01 +0000

    2012-01-03_01-44_1325555041

--- compress.rb
+++ compress.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby19 -WKU
 # require "front-compiler"
 # 
-# class FrontCompiler::HTMLCompactor
-# 	  def remove_trailing_spaces(source)
-# 		source.gsub /\n\s*\n/, "\n"
-# 		end
-# end
+class FrontCompiler::HTMLCompactor
+	  def remove_trailing_spaces(source)
+		source.gsub /\n\s*/, "\n"
+		end
+end
 
-# c = FrontCompiler.new
-# Dir.glob('_compiled/**/*.{html}').each do |f|
-# 	compressed = c.compact_file(f)
-# 	File.open(f, "w") { |file| file.write compressed }
-# end
+c = FrontCompiler.new
+Dir.glob('_compiled/**/*.{html}').each do |f|
+	compressed = c.compact_file(f)
+	File.open(f, "w") { |file| file.write compressed }
+end
